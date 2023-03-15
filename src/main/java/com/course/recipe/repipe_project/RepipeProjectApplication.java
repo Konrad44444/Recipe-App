41930082d1c49ddc9586686b4d06340daac0b693
Konrad44444 <konrad_warzecha@wp.pl>
2023-03-15 19:24:04 +0100

    create

---
+++ src/main/java/com/course/recipe/repipe_project/RepipeProjectApplication.java
@@ -0,0 +1,13 @@
+package com.course.recipe.repipe_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepipeProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RepipeProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/course/recipe/repipe_project/RepipeProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.course.recipe.repipe_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RepipeProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
