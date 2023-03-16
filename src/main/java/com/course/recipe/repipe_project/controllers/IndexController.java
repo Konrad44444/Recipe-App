8c7847d9a9cf6b20ee7da456e1fa8177f65abbcd
Konrad44444 <konrad_warzecha@wp.pl>
2023-03-16 19:30:10 +0100

    JPA query methods

--- src/main/java/com/course/recipe/repipe_project/controllers/IndexController.java
+++ src/main/java/com/course/recipe/repipe_project/controllers/IndexController.java
@@ -1,13 +1,37 @@
 package com.course.recipe.repipe_project.controllers;
 
+import java.util.Optional;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.course.recipe.repipe_project.domain.Category;
+import com.course.recipe.repipe_project.domain.UnitOfMeasure;
+import com.course.recipe.repipe_project.repositories.CategoryRepository;
+import com.course.recipe.repipe_project.repositories.UnitOfMeasureRepository;
+
 @Controller
 public class IndexController {
+
+    private CategoryRepository categoryRepository;
+    private UnitOfMeasureRepository unitOfMeasureRepository;
+
+
+    public IndexController(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
+        this.categoryRepository = categoryRepository;
+        this.unitOfMeasureRepository = unitOfMeasureRepository;
+    }
+
     
     @RequestMapping({"", "/", "/index", "/index.html"})
     public String getIndexPage() {
+
+        Optional<Category> categoryOptional = categoryRepository.findByDescription("American");
+        Optional<UnitOfMeasure> unitOfMeasureOptional = unitOfMeasureRepository.findByDescription("Teaspoon");
+
+        System.out.println("Cat id is: " + categoryOptional.get().getId());
+        System.out.println("UOM id is: " + unitOfMeasureOptional.get().getId());
+
         return "index";
     }
 }
