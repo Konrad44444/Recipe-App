f486239c9f194431aba67daa42956bf76d6ff084
Konrad44444 <konrad_warzecha@wp.pl>
2023-03-19 11:21:59 +0100

    create a recipe form and controller

--- src/main/java/com/course/recipe/repipe_project/controllers/RecipeController.java
+++ src/main/java/com/course/recipe/repipe_project/controllers/RecipeController.java
@@ -2,9 +2,12 @@ package com.course.recipe.repipe_project.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.course.recipe.repipe_project.commands.RecipeCommand;
 import com.course.recipe.repipe_project.services.RecipeService;
 
 
@@ -22,6 +25,21 @@ public class RecipeController {
         model.addAttribute("recipe", recipeService.findById(Long.parseLong(id)));
 
         return "recipe/show";
+    }
+
+    @RequestMapping("recipe/new")
+    public String newRecipe(Model model) {
+        model.addAttribute("recipe", new RecipeCommand());
+
+        return "recipe/recipeform";
+    }
+
+    //after adding show recipe
+    @PostMapping("/recipe")
+    public String saveOrUpdate(@ModelAttribute RecipeCommand command) {
+        RecipeCommand savedCommand = recipeService.saveRecipeCommand(command);
+
+        return "redirect:/recipe/show/" + savedCommand.getId();
     }
 
 }
