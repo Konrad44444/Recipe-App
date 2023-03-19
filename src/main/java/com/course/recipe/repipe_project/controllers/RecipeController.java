931a30d716cd60e32f46cd533c627a414b12ff2d
Konrad44444 <konrad_warzecha@wp.pl>
2023-03-19 16:16:08 +0100

    update recipe

--- src/main/java/com/course/recipe/repipe_project/controllers/RecipeController.java
+++ src/main/java/com/course/recipe/repipe_project/controllers/RecipeController.java
@@ -20,7 +20,7 @@ public class RecipeController {
         this.recipeService = recipeService;
     }
 
-    @RequestMapping("/recipe/show/{id}") //id is variable
+    @RequestMapping("/recipe/{id}/show") //id is variable
     public String showById(@PathVariable String id, Model model) {
         model.addAttribute("recipe", recipeService.findById(Long.parseLong(id)));
 
@@ -39,7 +39,14 @@ public class RecipeController {
     public String saveOrUpdate(@ModelAttribute RecipeCommand command) {
         RecipeCommand savedCommand = recipeService.saveRecipeCommand(command);
 
-        return "redirect:/recipe/show/" + savedCommand.getId();
+        return "redirect:/recipe/" + savedCommand.getId() + "/show";
+    }
+
+    @RequestMapping("recipe/{id}/update")
+    public String updateRecipe(@PathVariable String id, Model model) {
+        model.addAttribute("recipe", recipeService.findCommandById(Long.valueOf(id)));
+
+        return "recipe/recipeform";
     }
 
 }
