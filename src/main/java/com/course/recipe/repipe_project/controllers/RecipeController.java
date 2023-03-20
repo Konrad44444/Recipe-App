d1fd3e72b6fb144d2d1359e52c80cc58d3fec05c
Konrad44444 <konrad_warzecha@wp.pl>
2023-03-20 13:33:44 +0100

    delete recipe

--- src/main/java/com/course/recipe/repipe_project/controllers/RecipeController.java
+++ src/main/java/com/course/recipe/repipe_project/controllers/RecipeController.java
@@ -2,6 +2,7 @@ package com.course.recipe.repipe_project.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -20,6 +21,7 @@ public class RecipeController {
         this.recipeService = recipeService;
     }
 
+    @GetMapping
     @RequestMapping("/recipe/{id}/show") //id is variable
     public String showById(@PathVariable String id, Model model) {
         model.addAttribute("recipe", recipeService.findById(Long.parseLong(id)));
@@ -27,6 +29,7 @@ public class RecipeController {
         return "recipe/show";
     }
 
+    @GetMapping
     @RequestMapping("recipe/new")
     public String newRecipe(Model model) {
         model.addAttribute("recipe", new RecipeCommand());
@@ -42,11 +45,20 @@ public class RecipeController {
         return "redirect:/recipe/" + savedCommand.getId() + "/show";
     }
 
+    @GetMapping
     @RequestMapping("recipe/{id}/update")
     public String updateRecipe(@PathVariable String id, Model model) {
         model.addAttribute("recipe", recipeService.findCommandById(Long.valueOf(id)));
 
         return "recipe/recipeform";
+    }
+
+    @GetMapping
+    @RequestMapping("recipe/{id}/delete")
+    public String deleteById(@PathVariable String id) {
+        recipeService.deleteById(Long.parseLong(id));
+
+        return "redirect:/";
     }
 
 }
--- src/test/java/com/course/recipe/repipe_project/services/RecipeServiceImplTest.java
+++ src/test/java/com/course/recipe/repipe_project/services/RecipeServiceImplTest.java
@@ -73,5 +73,19 @@ public class RecipeServiceImplTest {
 
         verify(recipeRepository, times(1)).findAll(); //recipe repository was called only once [used by recipeService.getRecipes()]
     }
+
+    @Test
+    public void testDeleteById() throws Exception {
+        //given
+        Long idToDelete = 2L;
+
+        //when
+        recipeService.deleteById(idToDelete);
+
+        //no 'when', since method has void return type
+
+        //then
+        verify(recipeRepository, times(1)).deleteById(anyLong());
+    }
 }
     
